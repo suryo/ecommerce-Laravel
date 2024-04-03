c645e560116e09c4d1f8279cd04c90c1759fb455
suryo <suryoatm@gmail.com>
2024-04-03 09:50:28 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\DashboardController;
+
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// use App\Http\Controllers\Api\LogoutController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('login', [LoginController::class, 'index']);
+
+Route::apiResource('/category', CategoryController::class);
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+    Route::get('/logout', [LogoutController::class, 'index']);
+
+});
